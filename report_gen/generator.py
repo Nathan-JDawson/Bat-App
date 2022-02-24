866772c186af5158e87f31afa2419b728125dce6
MrTubbz <nathadawson123@hotmail.co.uk>
2022-02-24 12:58:26 +0000

    File not working with images currently

--- report_gen/generator.py
+++ report_gen/generator.py
@@ -1,13 +1,13 @@
 from typing import Dict
 import traceback
-from docx.shared import Mm
+from docx.shared import Mm # type: ignore
 from docxtpl import DocxTemplate, InlineImage # type: ignore
 import json
-from PIL import Image
+from PIL import Image # type: ignore
 
 def open_json(filename: str) -> Dict:
     with open(filename) as f:
-        data = json.load(f)
+        data: Dict = json.load(f)
     # endwith
 
     return data
@@ -22,10 +22,10 @@ if __name__ == "__main__":
         # import the template doc
         template = DocxTemplate("../report_gen/simply_ecology_template.docx")
 
-        filepath: str = "../report_gen/img/" + json_data["filenames"][0]
-        img = Image.open(filepath)
-        img = img.save(filepath)
-        json_data["image_external_wall_1"] = InlineImage(template, filepath, height=Mm(60))
+        # filepath: str = "../report_gen/img/" + json_data["filenames"][0]
+        # img = Image.open(filepath)
+        # img = img.save(filepath)
+        # json_data["image_external_wall_1"] = InlineImage(template, filepath, height=Mm(60))
 
         # render new doc
         template.render(json_data, autoescape=True)
